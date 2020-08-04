c953b1fa96356b626ebb08fb7c115634f3a37f94
duanyytop <duanyytop@gmail.com>
2020-08-04 11:10:40 +0800

    chore: Update cells filter method

--- src/utils.js
+++ src/utils.js
@@ -88,7 +88,7 @@ const getRawTxTemplate = () => {
 
 const groupCells = cells => {
   return {
-    emptyCells: cells.filter(cell => cell.output_data === "0x"),
+    emptyCells: cells.filter(cell => !cell.output_data || cell.output_data === "0x"),
     filledCells: cells.filter(cell => cell.output_data !== "0x")
   };
 };
